144706a9d8c2ac1fa873a1a59b15163bdbf3e2ec
tiao1409 <58492402+tiao1409@users.noreply.github.com>
2021-10-11 21:32:25 -0300

    Feito botao Excluir e Adicionar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'motoristas',
+    loadChildren:() => import ('./motoristas/motoristas.module').then(m => m.MotoristasModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
